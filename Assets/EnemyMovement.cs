c24387b89913231337db78b41ac92b9c0c4e3c7b
ydmsnd <xxpriscc@yahoo.com.au>
2023-07-26 12:19:45 +1000

    enemy2

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.U2D;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
     Rigidbody2D enemyRB;
+    public float speed;
+    bool isMovingRight = true;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +19,23 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(isMovingRight)
+        {
+            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+        }
+        else
+        {
+            transform.position -= new Vector3(speed, 0, 0) * Time.deltaTime;
+        }
 
-        transform.position = transform.position + new Vector3(Speed, 0, 0) * Time.deltaTime;
+        if(transform.position.x >= 10)
+        {
+            isMovingRight = false;
+        }
 
-        if(transform.position.x > 
+        if (transform.position.x <= -10)
+        {
+            isMovingRight = true;
+        }
     }
 }
